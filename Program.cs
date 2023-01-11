0c9b13c6c4abd407be1d1e29f6822dcddb344c40
kulla <kujakobs@tktk.ee>
2023-01-11 10:07:02 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// This is a comment to my code
+
+// rakendus küsib kasutajal sisestada tema nime
+// rakendus tervitab kasjutajat nimepidi
+
+Console.WriteLine(" Enter Your Name:");
+string userName = Console.ReadLine();
+
+
+Console.WriteLine("hello" + " , " + userName + "!");
+
+Console.WriteLine($"hello, {userName} !");
